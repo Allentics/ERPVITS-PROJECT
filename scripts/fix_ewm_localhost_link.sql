9456407acc2b87329be77234ecee209078db0990
Allentics <aitswebdev@gmail.com>
2026-03-11 15:29:38 +0530

    clean up temporary fix scripts

--- scripts/fix_ewm_localhost_link.sql
+++
@@ -1,4 +0,0 @@
--- Fix the localhost link in the SAP EWM Yard Management blog post
-UPDATE public.blog_posts 
-SET content = REPLACE(content, 'http://localhost:3000/courses/sap-ewm', '/courses/sap-ewm')
-WHERE id = 'understanding-the-process-flow-of-sap-ewm-yard-management';
