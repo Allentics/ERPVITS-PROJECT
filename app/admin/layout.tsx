be8cf5033816e6f8529280c24f3e8c8461c450cb
Allentics <aitswebdev@gmail.com>
2026-03-11 17:17:39 +0530

    fix: import blog.css in admin layout for correct preview styling

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -19,6 +19,7 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 import { supabase } from '@/lib/supabase';
+import '../blog/blog.css';
 
 function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
